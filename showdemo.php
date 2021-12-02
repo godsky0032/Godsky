7ed55519fd4f2f29f8cebc332a35157460b35d94
godsky0032 <zzzzzzv080@gmail.com>
2021-12-02 16:17:33 +0700

    godsky

--- showdemo.php
+++ showdemo.php
@@ -16,12 +16,13 @@ if (!$conn) {
 }
 
 $sql = "SELECT * FROM demo";
+echo $sql;
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " adress" . $row["address"]. "<br>";
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " address" . $row["address"]. "<br>";
     }
 } else {
     echo "0 results";
