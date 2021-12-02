ddfb9f6c021d4a9b45ae09de8e21baf2bda69e3d
godsky0032 <zzzzzzv080@gmail.com>
2021-12-02 16:38:23 +0700

    godsky

--- showdemo.php
+++ showdemo.php
@@ -10,7 +10,7 @@ $dbname = "it64309010002";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-mysqil_set_charset($conn,"utf8");
+mysqli_set_charset($conn,"utf8");
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
